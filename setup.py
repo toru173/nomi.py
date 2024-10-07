9fbfd9bec491fea398a0db348ad678772e34f84b
toru173 <toru173+github@gmail.com>
2024-10-07 18:17:41 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nomi.py',
-    version='0.0.1'
+    version='0.0.1',
     author='toru173',
     description='A Python module for interacting with the Nomi API',
     long_description=open('README.md').read(),
